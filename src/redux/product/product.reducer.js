4af4fa4dd025ba6157d8b698f6cab0d6d8c3780a
jatin77 <clockstopfountain@gmail.com>
2020-11-30 07:58:01 +0530

    img fix

--- src/redux/product/product.reducer.js
+++ src/redux/product/product.reducer.js
@@ -8,7 +8,8 @@ const INITIAL_STATE = {
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
       price: 12,
       quantity: 23,
-      image: "https://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Wireless/Apple/iPhone12/RiverImages/PC/IN_r1308-r1309_Marketing_Page_L_FFH-1500_01._CB419225211_.jpg",
       id: 1,
     },
     {
@@ -35,7 +36,8 @@ const INITIAL_STATE = {
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
       price: 22,
       quantity: 15,
-      image: "https://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Wireless/Apple/iPhone12/RiverImages/PC/IN_r1308-r1309_Marketing_Page_L_FFH-1500_01._CB419225211_.jpg",
       id: 4,
     },
   ],
